7e34af22d0f7cfc814f679a65cdc2dec35b6a023
Asus <nushrat.j01@gmail.com>
2020-08-07 20:58:46 +0600

    fragments and comunity

---
+++ app/src/main/java/com/example/hope/ComunityFragment.java
@@ -0,0 +1,43 @@
+package com.example.hope;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+
+public class ComunityFragment extends Fragment {
+
+
+    Button Addpost;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_community, container, false);
+
+
+        Addpost = v.findViewById(R.id.addPostbutton);
+
+        Addpost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getContext(),AddingPost.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        return v;
+
+    }
+
+}
