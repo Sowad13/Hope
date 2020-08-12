e1a513d04c5059894d379b7d360eb0c8725dcb81
Asus <nushrat.j01@gmail.com>
2020-08-12 23:50:59 +0600

    fragments and community

--- app/src/main/java/com/example/hope/ComunityFragment.java
+++ app/src/main/java/com/example/hope/ComunityFragment.java
@@ -5,17 +5,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ComunityFragment extends Fragment {
 
 
-    Button Addpost;
+    FloatingActionButton floatingActionButton;
+    RecyclerView postRecyclerview;
+    post_adapter Adapterpost;
+    List<detailpost> mData;
+
+
 
     @Nullable
     @Override
@@ -23,16 +35,46 @@ public class ComunityFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_community, container, false);
 
 
-        Addpost = v.findViewById(R.id.addPostbutton);
-
-        Addpost.setOnClickListener(new View.OnClickListener() {
+       floatingActionButton = v.findViewById(R.id.fabbutton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(getContext(),Adding_post.class);
                 startActivity(intent);
+
             }
         });
+
+
+        postRecyclerview = v.findViewById(R.id.recycler);
+        postRecyclerview.setHasFixedSize(true);
+        mData = new ArrayList<>();
+
+
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"OnePlus 6T Camera Review:","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.creat,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"hi","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.owlf,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"OnePlus 6T Camera Review:","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.creat,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"No","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.hope,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"OnePlus 6T Camera Review:","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.creat,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"go","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.owlf,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"OnePlus 6T Camera Review:","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.creat,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"Hello","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.hope,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"OnePlus 6T Camera Review:","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.creat,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"hi","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.owlf,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"OnePlus 6T Camera Review:","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.creat,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"No","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.hope,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"OnePlus 6T Camera Review:","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.creat,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"go","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.owlf,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"OnePlus 6T Camera Review:","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.creat,0));
+        mData.add(new detailpost(detailpost.TEXT_TYPE,"Hello","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",R.drawable.owlf,0));
+
+
+        Adapterpost = new post_adapter (getActivity(),mData);
+        postRecyclerview.setAdapter(Adapterpost);
+        postRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
 
 
 
