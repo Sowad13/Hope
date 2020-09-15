4de91273f632c54c66f612ea800c9b3ffe10c09b
sowad <mejbahurrahman13@gmail.com>
2020-09-15 15:10:21 +0600

    no crash

--- app/src/main/java/com/example/hope/UserChatActivity.java
+++ app/src/main/java/com/example/hope/UserChatActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
+import com.example.hope.Model.Users;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
--- app/src/main/java/com/example/hope/UsersFragment.java
+++ app/src/main/java/com/example/hope/UsersFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.hope.Model.Users;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UsersFragment extends Fragment {
+
 
     private RecyclerView recyclerView;
 
@@ -54,7 +56,7 @@ public class UsersFragment extends Fragment {
         recyclerView.setAdapter(userAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        readUsers();
+        //readUsers();
 
         return view;
 
@@ -63,7 +65,7 @@ public class UsersFragment extends Fragment {
     private void readUsers() {
 
         final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
